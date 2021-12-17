b4f3bf49a946183eed4c8494ad3e26e6e15f894b
Yash Joshi <yashjoshi3995@gmail.com>
2021-12-18 00:24:35 +0530

    Minor Update: Added Solution class object.

--- TestApplication/TestApplication.cpp
+++ TestApplication/TestApplication.cpp
@@ -3,10 +3,21 @@
 
 #include <iostream>
 #include "myHeader.h"
+#include "solution.h"
 
 
 int main()
 {
     HELLO("Yash");
+
+    Solution obj;
+    std::vector<int> pascalRow;
+    pascalRow = obj.getRow(10);
+
+    for (int i = 0; i < pascalRow.size(); i++)
+        std::cout << pascalRow.at(i) << " ";
+    std::cout << "\n";
+
+
     return 0;
 }
